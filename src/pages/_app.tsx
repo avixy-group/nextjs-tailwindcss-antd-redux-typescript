b1fb6433eebecda815b84cc94d14c2d6c2d2b206
Riyan Saputra <alamat.emailnya.riyan@gmail.com>
2023-04-28 16:48:45 +0700

    build: add redux and setup rtk query example

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,19 +1,28 @@
-import "antd/dist/reset.css";
-import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
+// TailwindCSS
+import "@/styles/globals.css";
+
+// AntD
 import { ConfigProvider } from "antd";
+import "antd/dist/reset.css";
+
+// Redux
+import { Provider } from "react-redux";
+import { store } from "../redux/store";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ConfigProvider
-      theme={{
-        token: {
-          colorPrimary: "#6b46c1",
-        },
-      }}
-    >
-      <Component {...pageProps} />
-    </ConfigProvider>
+    <Provider store={store}>
+      <ConfigProvider
+        theme={{
+          token: {
+            colorPrimary: "#6b46c1",
+          },
+        }}
+      >
+        <Component {...pageProps} />
+      </ConfigProvider>
+    </Provider>
   );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,15 @@
-import { Button, DatePicker } from 'antd';
+import { usePostsQuery } from "@/apis/example";
+import { Button, DatePicker } from "antd";
 
 export default function Home() {
+  const { data, isLoading, error } = usePostsQuery();
+
+  console.log(data, isLoading, error);
+
   return (
     <>
       <Button type="primary">Primary Button</Button>
       <DatePicker />
     </>
-  )
+  );
 }
