76069e3e53149b717dd9a38a6a42e2125008a641
Riyan Saputra <alamat.emailnya.riyan@gmail.com>
2023-05-12 11:25:14 +0700

    build: fix tailwindcss reset style

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: "class",
   content: ["./src/**/*.{tsx,mdx}"],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
-  },
-  plugins: [],
   corePlugins: {
     preflight: false,
   },
